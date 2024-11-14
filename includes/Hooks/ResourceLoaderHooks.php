57ef62f4306a21be623c1e0393286d9a38cad756
Aylong <alexanderkitsa@gmail.com>
2024-11-14 10:34:37 +0200

    Citizen note addition

--- includes/Hooks/ResourceLoaderHooks.php
+++ includes/Hooks/ResourceLoaderHooks.php
@@ -2,7 +2,7 @@
 /**
  * Citizen - A responsive skin developed for the Star Citizen Wiki
  *
- * This file is part of Citizen.
+ * This file is part of Citizen. Ported/Copypasted for TGUI
  *
  * Citizen is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
