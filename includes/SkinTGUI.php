9f4aa43709249c53bd8809db8ada2e246811f644
Aylong <alexanderkitsa@gmail.com>
2025-03-10 06:21:28 +0200

    Remove important blur & third blur preference

--- includes/SkinTGUI.php
+++ includes/SkinTGUI.php
@@ -208,6 +208,7 @@ class SkinTGUI extends SkinMustache {
 		$skinTheme->setSkinTheme( $options );
 
 		// Clientprefs feature handling
+		$this->addClientPrefFeature( 'tgui-feature-theme-style', 'default' );
 		$this->addClientPrefFeature( 'tgui-feature-blur', 'enabled' );
 		$this->addClientPrefFeature( 'tgui-feature-reduced-motion', 'disabled' );
 		$this->addClientPrefFeature( 'tgui-feature-holidays', 'enabled' );
