ff2a6bbaee6a3de356f0f6377065dfe32b56d17e
Aylong <alexanderkitsa@gmail.com>
2025-05-06 09:16:42 +0300

    Remove HONK (For safety)

--- resources/skins.tgui.holidays/scripts/April Fools.js
+++ resources/skins.tgui.holidays/scripts/April Fools.js
@@ -2,29 +2,3 @@ setInterval(() => {
   let hue = (parseInt(getComputedStyle(document.documentElement).getPropertyValue('--primary-hue')) || 0) + 10;
   document.documentElement.style.setProperty('--primary-hue', hue % 360);
 }, 200);
-
-function waitForButton() {
-  const observer = new MutationObserver(() => {
-    const container = document.getElementById('skin-client-prefs-tgui-feature-holidays');
-    if (!container) return;
-
-    const buttons = container.querySelectorAll('.tgui-client-prefs-radio__label');
-    if (buttons.length < 2) return;
-
-    const button = buttons[1];
-    observer.disconnect();
-    button.addEventListener(
-      'click',
-      () => {
-        new Audio('/skins/TGUI/resources/skins.tgui.holidays/assets/HONK.mp3').play();
-      },
-      { once: true },
-    );
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    observer.observe(document.body, { childList: true, subtree: true });
-  });
-}
-
-waitForButton();
